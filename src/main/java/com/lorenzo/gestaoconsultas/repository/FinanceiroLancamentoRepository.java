b87a65cfec97aaacb705c15c878bbbc66975632e
lorenzo04andreoli <lorenzo04andreoli@gmail.com>
2026-06-04 19:57:39 -0300

    feat(financeiro): adiciona modelo de lancamento

---
+++ src/main/java/com/lorenzo/gestaoconsultas/repository/FinanceiroLancamentoRepository.java
@@ -0,0 +1,7 @@
+package com.lorenzo.gestaoconsultas.repository;
+
+import com.lorenzo.gestaoconsultas.entity.FinanceiroLancamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FinanceiroLancamentoRepository extends JpaRepository<FinanceiroLancamento, Long> {
+}
