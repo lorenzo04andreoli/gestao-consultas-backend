e5a2c7d2646d2a0e87826bf7a969926236390275
lorenzo04andreoli <lorenzo04andreoli@gmail.com>
2026-06-04 20:02:52 -0300

    feat(financeiro): cria dto de lancamento

---
+++ src/main/java/com/lorenzo/gestaoconsultas/dto/FinanceiroResumoResponseDto.java
@@ -0,0 +1,41 @@
+package com.lorenzo.gestaoconsultas.dto;
+
+import java.math.BigDecimal;
+
+public class FinanceiroResumoResponseDto {
+
+    private BigDecimal recebidoMes;
+    private BigDecimal aReceber;
+    private Long pendentes;
+    private Long pagas;
+    private Long canceladas;
+
+    public FinanceiroResumoResponseDto(BigDecimal recebidoMes, BigDecimal aReceber,
+                                       Long pendentes, Long pagas, Long canceladas) {
+        this.recebidoMes = recebidoMes;
+        this.aReceber = aReceber;
+        this.pendentes = pendentes;
+        this.pagas = pagas;
+        this.canceladas = canceladas;
+    }
+
+    public BigDecimal getRecebidoMes() {
+        return recebidoMes;
+    }
+
+    public BigDecimal getAReceber() {
+        return aReceber;
+    }
+
+    public Long getPendentes() {
+        return pendentes;
+    }
+
+    public Long getPagas() {
+        return pagas;
+    }
+
+    public Long getCanceladas() {
+        return canceladas;
+    }
+}
